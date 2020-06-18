ef510b392df4886a88f2579a3d9483104dbc1f5a
Flatren <45997535+Flatren@users.noreply.github.com>
2020-06-18 12:39:41 +0500

    up

--- WebApiAntiContr/Controllers/LoginController.cs
+++ WebApiAntiContr/Controllers/LoginController.cs
@@ -14,34 +14,42 @@ namespace WebApiAntiContr.Controllers
     public class LoginController : ApiController
     {
         // GET api/<controller>/5
-        public object Get(string email,string code)
-        {
-            DBDataContext db = new DBDataContext();
+        //public object Get(string email,string code)
+        //{
+        //    DBDataContext db = new DBDataContext();
             
-            List<User> user = (from re in db.Users where re.Email == email && re.UserHesh == code select re).ToList();
+        //    List<User> user = (from re in db.Users where re.Email == email && re.UserHesh == code select re).ToList();
 
-            if (user.Count != 0)
-            {
-                return user[0].UserToken;
-            }
+        //    if (user.Count != 0)
+        //    {
+        //        return user[0].UserToken;
+        //    }
 
-            return new Error() {error = 0, description = "Не найден пользователь" };
-        }
+        //    return new Error() {error = 0, description = "Не найден пользователь" };
+        //}
 
         // POST api/<controller>
-        public object Post([FromBody]string email, [FromBody]string code)
+        
+        public object Post([FromBody]Newtonsoft.Json.Linq.JToken value)
         {
+            ApiLogin apiLogin=JsonConvert.DeserializeObject<ApiLogin>(value.ToString());
+            if(apiLogin==null)
+            {
+                return new Error() { error = 12, description = value.ToString() };
+            }
+
             DBDataContext db = new DBDataContext();
 
-            List<User> user = (from re in db.Users where re.Email == email && re.UserHesh == code select re).ToList();
+            List<User> user = (from re in db.Users where re.Email == apiLogin.email && re.UserHesh == apiLogin.code select re).ToList();
 
             if (user.Count != 0)
             {
                 return user[0].UserToken;
             }
 
-            return new Error() { error = 0, description = "Не найден пользователь" };
+            return new Error() { error = 0, description = JsonConvert.SerializeObject(apiLogin) };
         }
+        
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
--- Антиконтрафакт/Controllers/HomeController.cs
+++ Антиконтрафакт/Controllers/HomeController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Mvc;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
 
 namespace Антикотрафакт.Controllers
 {
-    public class HomeController : Controller
+    public  class HomeController : Controller
     {
+        private static HttpClient client = new HttpClient();
+
+        private static int p = 0;
         public ActionResult Index()
         {
             return View();
@@ -26,8 +38,36 @@ namespace Антикотрафакт.Controllers
 
             return View();
         }
+        
         public ActionResult Authorization()
         {
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        public  ActionResult Authorization(string Email, string Password)
+        {
+            p++;
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                //client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                values.Add("email", Email);
+                values.Add("code", Password);
+                var json = JsonConvert.SerializeObject(values);
+                //string url = @"http://godnext-001-site1.btempurl.com/api/Login";
+                string url = @"http://localhost:51675/api/Login";
+                byte[] utf8Bytes = client.UploadValues(url, "POST", values);
+                Encoding utf8 = Encoding.GetEncoding("UTF-8");
+                Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+                byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
+                var responseString = win1251.GetString(win1251Bytes);
+                ViewBag.Name = responseString;
+            }
+
+
             return View();
         }
     }
